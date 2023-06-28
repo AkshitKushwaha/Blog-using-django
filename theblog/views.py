ff3d9ff7c11a862ac94604f64df499571e2d4a2a
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2023-06-28 19:09:47 +0530

    This is a blog website made using django

--- theblog/views.py
+++ theblog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 # Create your views here.
+
+# Using cass based views
 
 #def home(request):
     #return render(request, 'home.html', {})
@@ -14,3 +16,14 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
+    #fields = ('title', 'body')
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'update_post.html'
+    fields = ['title', 'title_tag', 'body']
