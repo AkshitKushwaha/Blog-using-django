b39cfed3b326ac881098abf0fbb0d7edd2603fdc
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2023-06-29 09:49:47 +0530

    Added ordering of blogs

--- theblog/views.py
+++ theblog/views.py
@@ -13,6 +13,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 class ArticleDetailView(DetailView):
     model = Post
