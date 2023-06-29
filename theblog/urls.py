55a3d2eab460e425f01e8e203770bc8e69d60958
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2023-06-29 14:42:49 +0530

    Added Categories pages.

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
 
 # Note:- <int:pk> is used to get the primary key of the post. That is to identify a unique blog post.
 # Note:- The name of the path is used in the html file to link to the path.
@@ -15,5 +15,8 @@ urlpatterns = [
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
     path('add_category/', AddCategoryView.as_view(), name='add_category'),
+    path('category/<str:cats>/', CategoryView, name='category'),
 
 ]
+
+#we can call key any thing here eg. <str:cats> for categories.
--- theblog/views.py
+++ theblog/views.py
@@ -17,6 +17,11 @@ class HomeView(ListView):
     ordering = ['-post_date']
     #ordering = ['-id']
 
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats)
+    return render(request, 'categories.html', {'cats': cats, 'category_posts': category_posts})
+
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
