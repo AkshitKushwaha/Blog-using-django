e99df8054a1f1c7088507f75a348c38f6e095497
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2023-06-29 10:26:12 +0530

    Added a user authentication system to the blog

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'theblog',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
