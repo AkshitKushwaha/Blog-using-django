86222bbfeef306a8546042757847931a03c9060d
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2023-06-29 14:56:35 +0530

    Fixed Bugs in Categories

--- theblog/views.py
+++ theblog/views.py
@@ -18,8 +18,8 @@ class HomeView(ListView):
     #ordering = ['-id']
 
 def CategoryView(request, cats):
-    category_posts = Post.objects.filter(category=cats)
-    return render(request, 'categories.html', {'cats': cats, 'category_posts': category_posts})
+    category_posts = Post.objects.filter(category=cats.replace('-', ' '))
+    return render(request, 'categories.html', {'cats': cats.title().replace('-',' '), 'category_posts': category_posts})
 
 
 class ArticleDetailView(DetailView):
