892594d231b20d9f6e48223c1b3d535f1201108c
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2023-06-30 15:31:28 +0530

    Added like and unlike functionality

--- theblog/views.py
+++ theblog/views.py
@@ -46,8 +46,15 @@ class ArticleDetailView(DetailView):
 
         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
         context["cat_menu"] = cat_menu
         context["total_likes"] = total_likes
+        context["liked"] = liked
+        #after adding context, we need to add it to the html file and use its functionality
         return context
 
 class AddPostView(CreateView):
@@ -81,17 +88,15 @@ class AddCategoryView(CreateView):
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
-
     #note:- post_id is the name of the like button
-    #liked = False
-    #if post.likes.filter(id=request.user.id).exists():
-        #post.likes.remove(request.user)
-        #liked = False
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
 
-    #else:
-    #post.likes.add(request.user)
-        #liked = True
+    else:
+        post.likes.add(request.user)
+        liked = True
     
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
